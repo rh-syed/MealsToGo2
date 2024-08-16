691cc507ca4e03da9e4d81bec560319238fbd22d
Syed <rh_syed@outlook.com>
2024-08-15 19:04:35 -0600

    Adding Styled Components

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,52 +1,29 @@
 import React, { useState } from "react";
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  StatusBar,
-  SafeAreaView,
-} from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import {
+  AndroidAdjustedSafeAreaView,
+  ListViewContainer,
+  RestaurantContainer,
+  SearchContainer,
+} from "../styles/restaurants.screen.styles";
 
 export const RestaurantsScreen = () => {
   const [searchQuery, setSearchQuery] = useState("");
   return (
-    <SafeAreaView style={styles.AndroidSafeArea}>
-      <View style={styles.container}>
-        <View style={styles.searchView}>
+    <AndroidAdjustedSafeAreaView>
+      <RestaurantContainer>
+        <SearchContainer>
           <Searchbar
             placeholder="Search"
             onChangeText={setSearchQuery}
             value={searchQuery}
           />
-        </View>
-        <View style={styles.listView}>
+        </SearchContainer>
+        <ListViewContainer>
           <RestaurantInfoCard />
-        </View>
-      </View>
-    </SafeAreaView>
+        </ListViewContainer>
+      </RestaurantContainer>
+    </AndroidAdjustedSafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    direction: "row",
-  },
-  searchView: {
-    flex: 0.1,
-    padding: 16,
-  },
-  listView: {
-    flex: 0.9,
-    backgroundColor: "red",
-    padding: 16,
-  },
-  AndroidSafeArea: {
-    flex: 1,
-    backgroundColor: "white",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
