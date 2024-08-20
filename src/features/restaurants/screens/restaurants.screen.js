98d8e3404bd7a9e88e96a5528ee96b4b05e06e09
Syed <rh_syed@outlook.com>
2024-08-20 16:37:35 -0600

    ADded ListView

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -5,8 +5,10 @@ import {
   AndroidAdjustedSafeAreaView,
   ListViewContainer,
   RestaurantContainer,
+  RestaurantList,
   SearchContainer,
 } from "../styles/restaurants.screen.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 export const RestaurantsScreen = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -21,7 +23,26 @@ export const RestaurantsScreen = () => {
           />
         </SearchContainer>
         <ListViewContainer>
-          <RestaurantInfoCard />
+          <RestaurantList
+            data={[
+              { name: 1 },
+              { name: 3 },
+              { name: 4 },
+              { name: 5 },
+              { name: 6 },
+              { name: 7 },
+              { name: 8 },
+              { name: 9 },
+              { name: 10 },
+              { name: 11 },
+            ]}
+            keyExtractor={(item) => item.name}
+            renderItem={() => (
+              <Spacer position="bottom" size="large">
+                <RestaurantInfoCard />
+              </Spacer>
+            )}
+          />
         </ListViewContainer>
       </RestaurantContainer>
     </AndroidAdjustedSafeAreaView>
--- src/features/restaurants/styles/restaurants.screen.styles.js
+++ src/features/restaurants/styles/restaurants.screen.styles.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Platform, SafeAreaView, StatusBar, View } from "react-native";
+import {
+  FlatList,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  View,
+} from "react-native";
 import styled from "styled-components";
 
 export const RestaurantContainer = styled(View)`
@@ -14,7 +20,6 @@ export const SearchContainer = styled(View)`
 
 export const ListViewContainer = styled(View)`
   flex: 0.9;
-  background-color: ${(props) => props.theme.colors.ui.quaternary};
   padding: ${(props) => props.theme.space[3]};
 `;
 
@@ -22,4 +27,10 @@ export const AndroidAdjustedSafeAreaView = styled(SafeAreaView)`
   flex: 1;
   background-color: ${(props) => props.theme.colors.ui.quaternary};
   padding-top: ${Platform.OS === "android" ? StatusBar.currentHeight : 0}px;
+`;
+
+export const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {},
+})`
+  background-color: ${(props) => props.theme.colors.ui.quaternary};
 `;
